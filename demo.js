af561bc86c0467a2d6ca1ff5d1c5f0f2372bf6e0
Maxwell <mxmoore31@gmail.com>
2026-02-08 11:38:13 -0500

    Changed the webpages name 1.0.

---
+++ demo.js
@@ -0,0 +1,6 @@
+// 1) Grab elements from the page
+// const loadBtn = document.getElementById("loadBtn");
+// const statusEl = document.getElementById("status");
+// const tempEl = document.getElementById("temp");
+
+// 2) API URL (Open-Meteo, no key needed)
